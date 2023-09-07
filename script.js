25d0b97dcb9f6e92662b54d02ac2d0d460a416bb
Hasir Ali <102877876+hasirali@users.noreply.github.com>
2023-09-07 10:36:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-// generate a random color
-const randomColor = function () {
-    const hex = "0123456789ABCDEF"
-    let color = '#'
-    for (let i = 0; i < 6; i++) {
-        color = color + hex[Math.floor(Math.random() * 16)]
-    }
-    return color
-};
+const insert = document.getElementById('insert');
 
-
-
-
-// start
-let intervalId;
-const bgcolor = function changeBgColor() {
-    document.body.style.backgroundColor = randomColor();
-}
-const startChangingColor = function () {
-    if (!intervalId) {
-        intervalId = setInterval(bgcolor, 1000)
-    }
-}
-
-
-
-
-
-// stop
-const stopChangingColor = function () {
-    clearInterval(intervalId);
-    intervalId = null;
-}
-
-
-document.querySelector('#start').addEventListener('click', startChangingColor)
-document.querySelector('#stop').addEventListener('click', stopChangingColor)
-
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+    <div class="color">
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>KeyCode</th>
+                <th>Code</th>
+            </tr>
+            <tr>
+                <td>${e.key === ' ' ? 'Space' : e.key}</td>
+                <td>${e.keyCode}</td>
+                <td>${e.code}</td>
+            </tr>
+        </table>
+    </div>`;
+});
